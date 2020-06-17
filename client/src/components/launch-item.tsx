ca704d695125b623bd6b6c48a805d4fb3041de61
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-17 12:59:31 +0100

    Refactor Launch-Item

--- client/src/components/launch-item.tsx
+++ client/src/components/launch-item.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Launch } from '../models/launch.model';
 
-export default function LaunchItem(props: {
-  key: number;
+export default function LaunchItem({
+  launch,
+}: {
   launch: Launch;
 }): JSX.Element {
-  console.log(props);
-  return <div>{props.launch.mission_name}</div>;
+  return <div>{launch.mission_name}</div>;
 }
