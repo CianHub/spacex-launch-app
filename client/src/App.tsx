1afdf066a15f65277141cc3735ce6371abc2301e
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-17 17:51:20 +0100

    Install and add react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Launches from './components/launches';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 import logo from './logo.png';
 
@@ -10,19 +12,21 @@ const client = new ApolloClient({ uri: 'http://localhost:5000/graphql' });
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img
-          src={logo}
-          alt="SpaceX Logo"
-          style={{
-            width: 150,
-            height: 100,
-            margin: 'auto 0',
-            filter: 'brightness(0) invert(1)',
-          }}
-        />
-        <Launches></Launches>
-      </div>
+      <Router>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX Logo"
+            style={{
+              width: 150,
+              height: 100,
+              margin: 'auto 0',
+              filter: 'brightness(0) invert(1)',
+            }}
+          />
+          <Route exact path="/" component={Launches}></Route>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
