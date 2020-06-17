aee8c43d5dbc18ad209106bf3cedb21679a4cfa7
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-17 12:23:46 +0100

    Add CORS

--- server.ts
+++ server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { schema } from './schema';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(
   '/graphql',
   graphqlHTTP({
