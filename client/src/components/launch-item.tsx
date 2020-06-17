3a436967bd084bdc5033482159aa865cb1b2bf60
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-17 13:02:22 +0100

    Add styling to launch-item

--- client/src/components/launch-item.tsx
+++ client/src/components/launch-item.tsx
@@ -6,5 +6,17 @@ export default function LaunchItem({
 }: {
   launch: Launch;
 }): JSX.Element {
-  return <div>{launch.mission_name}</div>;
+  return (
+    <div className="card card-body mb-3">
+      <div className="row">
+        <div className="col-md-9">
+          <h4>Mission: {launch.mission_name}</h4>
+          <p>Date: {launch.launch_date_local}</p>
+        </div>
+        <div className="col-md-3">
+          <button className="btn btn-secondary">Launch Details</button>
+        </div>
+      </div>
+    </div>
+  );
 }
