b9a5b37441293f44704343bae37bbf542cce659e
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-18 12:28:52 +0100

    Add new build, add procfile

---
+++ public/build/precache-manifest.d2ac7de7d24b1af48650e4e7f34863c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278182c251b052aa5d2d8311b53ff3d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1cb7126c75a015e3e6b",
+    "url": "/static/css/main.b83d05d2.chunk.css"
+  },
+  {
+    "revision": "556f6bc72c6f2382e367",
+    "url": "/static/js/2.11e46f5c.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.11e46f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1cb7126c75a015e3e6b",
+    "url": "/static/js/main.3ec07132.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
